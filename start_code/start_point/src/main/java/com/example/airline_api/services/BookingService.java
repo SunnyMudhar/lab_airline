67e80fe3696cd6d88d9e30f5bd54599b7284c5f6
Sunny <sunielmudhar@gmail.com>
2024-06-06 01:44:41 +0100

    Booking functionality

--- start_code/start_point/src/main/java/com/example/airline_api/services/BookingService.java
+++ start_code/start_point/src/main/java/com/example/airline_api/services/BookingService.java
@@ -1,6 +1,9 @@
 package com.example.airline_api.services;
 
 import com.example.airline_api.models.Booking;
+import com.example.airline_api.models.BookingDTO;
+import com.example.airline_api.models.Flight;
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,12 @@ public class BookingService {
     @Autowired
     BookingRepository bookingRepository;
 
+    @Autowired
+    PassengerService passengerService;
+
+    @Autowired
+    FlightService flightService;
+
     public void saveBooking(Booking booking) {
         bookingRepository.save(booking);
     }
@@ -24,5 +33,18 @@ public class BookingService {
 
     public Optional<Booking> findSingleBooking(Long id) {
         return bookingRepository.findById(id);
+    }
+
+    public Booking createBooking(BookingDTO bookingDTO) {
+        Passenger passenger = passengerService.findSinglePassenger(bookingDTO.getPassenger().getId()).get();
+        Flight flight = flightService.findSingleFlight(bookingDTO.getFlight().getId()).get();
+        Booking booking = new Booking(
+                flight,
+                passenger,
+                bookingDTO.getSeatNumber()
+                //bookingDTO.getMealPreference()
+        );
+        saveBooking(booking);
+        return booking;
     }
 }
