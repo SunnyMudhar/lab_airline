452ad086a50fc23cab07b7e9352bac6b08252ec6
Sunny <sunielmudhar@gmail.com>
2024-06-06 01:51:47 +0100

    Passenger Functionality

--- start_code/start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ start_code/start_point/src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,31 +1,41 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import com.example.airline_api.services.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/passengers")
 public class PassengerController {
 
-    // TODO: Display details of all passengers
+    @Autowired
+    PassengerService passengerService;
+
     @GetMapping
     public ResponseEntity<List<Passenger>> getAllPassengers(){
-        return null;
+        return new ResponseEntity<>(passengerService.getAllPassengers(), HttpStatus.OK);
     }
 
-    // TODO: Display specific passenger details
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Passenger> getPassengerById(){
-        return null;
+    public ResponseEntity<Passenger> getPassengerById(@PathVariable Long id){
+        Optional<Passenger> passenger = passengerService.findSinglePassenger(id);
+
+        if(passenger.isPresent()) {
+            return new ResponseEntity<>(passenger.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    // TODO: Add a new passenger
     @PostMapping
-    public ResponseEntity<Passenger> addNewPassenger(){
-        return null;
+    public ResponseEntity<Passenger> addNewPassenger(Passenger passenger){
+        passengerService.savePassenger(passenger);
+        return new ResponseEntity<>(passenger, HttpStatus.CREATED);
     }
 
 }
